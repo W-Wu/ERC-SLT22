a71ecf3f8e60b3c7007c8b408ee310ee03f17b40
W-Wu <102689817+W-Wu@users.noreply.github.com>
2022-03-30 19:17:53 +0100

    Update data_prep_organize-in-diag.py

--- data_prep_organize-in-diag.py
+++ data_prep_organize-in-diag.py
@@ -3,20 +3,20 @@ import json
 import numpy as np
 
 
-output_dir = './data/'
-order_file_path = './data/order.json'
+output_dir = '../data/'
+order_file_path = '../data/order.json'
 data_type = 'label_hard'
 f=open(order_file_path,'r')
 text=f.read()
 paras=json.loads(text)
 if data_type == 'label':
-    data_path = './data/IEMOCAP-softlabel-sum.npy'
+    data_path = '../data/IEMOCAP-softlabel-sum.npy'
 if data_type == 'label_hard':
-    data_path = './data/IEMOCAP-hardlabel.npy'
+    data_path = '../data/IEMOCAP-hardlabel.npy'
 elif data_type == 'w2v2':
-    data_path = './data/w2v2-ft-avg.npy'
+    data_path = '../data/w2v2-ft-avg.npy'
 elif data_type == 'bert':
-    data_path = './data/bert-base.npy'
+    data_path = '../data/bert-base.npy'
 data=np.load(data_path,allow_pickle=True).item()
 
 diag_dic={}
